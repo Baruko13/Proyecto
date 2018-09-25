7dc89db7b665044f30b11ad677482867a6a04399
Baruko13 <42653422+Baruko13@users.noreply.github.com>
2018-09-25 14:50:43 -0500

    Primer commit

---
+++ Piedra 2/Piedra 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Piedra 2
+//
+//  Created by Usuario invitado on 25/9/18.
+//  Copyright © 2018 Usuario invitado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
